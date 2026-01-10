c82f1705a0ac7fd9761afebe142c8996db46ebc9
Nataly Martínez <natalymmg@gmail.com>
2026-01-10 09:20:15 -0300

    alerta bootstrap

--- depositar.js
+++ depositar.js
@@ -1,5 +1,5 @@
   
-  // mostrar el saldoactual (Local Storage + jquery)
+  // mostrar el saldo actual (Local Storage + jquery)
 
 $(function () {
   let saldo = parseInt(localStorage.getItem("saldo")) || 60000;
@@ -7,6 +7,16 @@ $(function () {
   $('#saldoNav').text('$' + saldo);
 
 
+// alerta Bootstrap para confirmar que el depósito fue realizado con éxito.
+  
+  function mostrarAlerta(mensaje, tipo) {
+    $('#alert-container').html(`
+      <div class="alert alert-${tipo} alert-dismissible fade show" role="alert">
+        ${mensaje}
+        <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
+      </div>
+    `);
+  }
 
 });
 
@@ -20,18 +30,18 @@ $(function () {
 
   let saldo = 60000; 
 
-  btnDeposito.addEventListener("click", (e) => {
-    e.preventDefault();
+  // btnDeposito.addEventListener("click", (e) => {
+  //   e.preventDefault();
 
-    const monto = parseInt(inputMonto.value);
+  //   const monto = parseInt(inputMonto.value);
 
-    if (isNaN(monto) || monto <= 0) {
-      alert("Ingresa un monto válido");
-      return;
-    }
+  //   if (isNaN(monto) || monto <= 0) {
+  //     alert("Ingresa un monto válido");
+  //     return;
+  //   }
 
     // saldo = saldo + monto;
     // saldoNav.textContent = "$" + saldo;
 
-    inputMonto.value = "";
+    // inputMonto.value = "";
   });
